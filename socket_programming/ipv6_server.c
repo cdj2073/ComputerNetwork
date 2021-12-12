a74b38711e3df6801111e87584304ca6df7f9b8d
cdj2073 <ab8922@naver.com>
2021-12-13 04:18:51 +0900

    get token

--- socket_programming/ipv6_server.c
+++ socket_programming/ipv6_server.c
@@ -13,8 +13,8 @@ int main() {
     int sockfd, new_sd, client_len;
     struct sockaddr_in6 server, client;
 
-    int ipv4_sd;
-    struct sockaddr_in ipv4_client;
+    int ipv4_sd, ipv4_conn_sd;
+    struct sockaddr_in6 ipv4_server;
     char buf[BUFFER_SIZE];
 
     // Create socket
@@ -37,6 +37,26 @@ int main() {
         fprintf(stderr, "Listen Error\n");
     }
 
+
+    // IPv4
+    if ((ipv4_sd = socket(AF_INET6, SOCK_STREAM, 0)) == -1) {
+        fprintf(stderr, "Fail to create a socket - IPv4\n");
+        exit(1);
+    }
+    memset(&ipv4_server, 0x00, sizeof(struct sockaddr_in));
+    ipv4_server.sin6_family = AF_INET6;
+    ipv4_server.sin6_port = htons(SERVER_TCP_PORT);
+    ipv4_server.sin6_addr = in6addr_any;     // in6addr_any == htonl(INADDR_ANY)
+    if (bind(ipv4_sd, (struct sockaddr *)&ipv4_server, sizeof(ipv4_server)) == -1) {
+        fprintf(stderr, "Bind Error - IPv4\n");
+        exit(1);
+    }
+
+    if (listen(ipv4_sd, 5) == -1) {
+        fprintf(stderr, "Listen Error - IPv4\n");
+    }
+    if ((ipv4_new_sd = accept(ipv4_sd, (struct sockaddr *)&)))
+
     for (int i = 0; i < 5; i++) {
         client_len = sizeof(client);
         printf("clientlen : %d\n", client_len);
@@ -54,42 +74,6 @@ int main() {
         close(new_sd);
     }
 
-    // IPv4 client
-    /*
-    // Create socket
-    if ((ipv4_sd = socket(AF_INET, SOCK_STREAM, 0)) == -1) { 
-        fprintf(stderr, "Fail to create a socket\n");
-        exit(1);
-    }
-
-    // Bind an address to socket
-    memset(&server, 0x00, sizeof(struct sockaddr_in));
-    server.sin6_family = AF_INET6;
-    server.sin6_port = htons(SERVER_TCP_PORT);
-    server.sin6_addr = in6addr_any;     // in6addr_any == htonl(INADDR_ANY)
-    
-	//printf("ipv6 : %s\n", (char *)server.sin6_addr);
-    if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
-        fprintf(stderr, "Bind Error\n");
-        exit(1);
-    }
-
-    if (listen(sockfd, 5) == -1) {
-        fprintf(stderr, "Listen Error\n");
-    }
-*/
-    if ((ipv4_sd = accept(sockfd, (struct sockaddr *)&client, (socklen_t *)&client_len)) == -1) {
-        fprintf(stderr, "Accept Error\n");
-        exit(1);
-    }
-    printf("Accept IPv4 Client\n");
-    bzero(buf, BUFFER_SIZE);
-    while ((n = read(ipv4_sd, buf, BUFFER_SIZE)) > 0) {
-        buf[strlen(buf) - 2] = '\0';
-        printf("ipv4_sd : %d => %s..\n", ipv4_sd, buf);
-    }
-    close(ipv4_sd);
-    printf("Close client\n");
     close(sockfd);
 
     return (0);
