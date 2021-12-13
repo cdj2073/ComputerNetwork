5a4dd6404ad300b9bff2090278046c2023975d11
cdj2073 <ab8922@naver.com>
2021-12-13 21:57:23 +0900

    data transfer using message queue

--- socket_programming/ipv4_client.c
+++ socket_programming/ipv4_client.c
@@ -2,25 +2,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-//#include <netdb.h>  // gethostbyname()
 
 #define BUFFER_SIZE 1024
 #define SERVER_TCP_PORT 50000
-#define IPV6_TCP_PORT 45000
 
-// ipv6 주소 --------------------------------------------나중에 지우기
-// inet6 2001:0:c38c:c38c:84a:36d5:f1d9:337b
+void ipv6_conn(char *server_ipv6, int port);
 
 int main() {
-    int sockfd, n;
-    char *server_ip;//, *bp, sbuf[BUFFER_SIZE], rbuf[BUFFER_SIZE];
+    int sockfd, n, port_v6;
+    char *server_ip, *server_ipv6;
     struct sockaddr_in server_addr;
-    //struct sockaddr_in6 ipv6_server;
-    // char *dns = "ec2-3-17-53-130.us-east-2.compute.amazonaws.com";
-    
+    char buf[BUFFER_SIZE], tokens[BUFFER_SIZE];
+ 
     server_ip = "3.17.53.130";
     memset(&server_addr, 0x00, sizeof(struct sockaddr_in));
     server_addr.sin_family = AF_INET;
@@ -28,7 +26,7 @@ int main() {
     server_addr.sin_addr.s_addr = inet_addr(server_ip);
 
     // create client socket (IPv4, TCP)
-    if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
         fprintf(stderr, "Fail to create a socket\n");
         exit(1);
     }
@@ -42,30 +40,61 @@ int main() {
     // read / write
     printf("Connected\n");
 
-    char buf[BUFFER_SIZE];
     bzero(buf, BUFFER_SIZE);
-
     while ((n = read(sockfd, buf, BUFFER_SIZE)) > 0) {
         printf("%s", buf);
         bzero(buf, BUFFER_SIZE);
-
-        fgets(buf, BUFFER_SIZE, stdin);
+        if ((n = read(sockfd, buf, BUFFER_SIZE)) > 0) {
+            printf("%s", buf);
+            bzero(buf, BUFFER_SIZE);
+        }
+        read(0, buf, BUFFER_SIZE);
         write(sockfd, buf, strlen(buf));
+        if (strcmp(buf, "OK\n") == 0) {
+            sleep(10);
+            
+            typedef struct message {
+                long msg_type;
+                char data[BUFFER_SIZE];
+            } message;
+            
+            int msqid;
+            message msg;
+
+            if ((msqid = msgget(1234, IPC_CREAT|0666)) == -1) {
+                fprintf(stderr, "Error msgget\n");
+                exit(1);
+            }
+            if (msgrcv(msqid, &msg, sizeof(msg), 0, 0) == -1) {
+                fprintf(stderr, "Error msgrcv\n");
+                exit(1);
+            }
+            strcpy(tokens, msg.data);
+            tokens[strlen(tokens)] = 0x0a;
+            //printf("token : %s\n", tokens);
+            write(sockfd, tokens, strlen(tokens));
+            
+            if (msgctl(msqid, IPC_RMID, NULL) == -1) {
+                fprintf(stderr, "Error msgctl\n");
+                exit(1);
+            }
+            else {
+                //printf("success to get token!\n");
+                break;
+            }
+        }
+        
         bzero(buf, BUFFER_SIZE);
     }
     
-    /*
-    fgets(buf, BUFFER_SIZE, stdin);
-    //gets(buf);
-    write(sockfd, buf, strlen(buf));
-    bzero(buf, BUFFER_SIZE);
-
-    while ((n = read(sockfd, buf, BUFFER_SIZE - 1)) > 0) {
+    if ((n = read(sockfd, buf, BUFFER_SIZE)) > 0) {
         printf("%s", buf);
         bzero(buf, BUFFER_SIZE);
     }
-    */
-
+    if ((n = read(sockfd, buf, BUFFER_SIZE)) > 0) {
+        printf("%s", buf);
+        bzero(buf, BUFFER_SIZE);
+    }
     // close
     close(sockfd);
     return (0);
--- socket_programming/ipv6_server.c
+++ socket_programming/ipv6_server.c
@@ -3,22 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
+#include <sys/msg.h>
+#include <sys/ipc.h>
 
 #define BUFFER_SIZE 1024
 #define SERVER_TCP_PORT 45000
-#define IPV4_TCP_PORT 50000
 
 int main() {
-    int n;
+    int n, idx;
     int sockfd, new_sd, client_len;
     struct sockaddr_in6 server, client;
-
-    int ipv4_sd, ipv4_conn_sd;
-    struct sockaddr_in6 ipv4_server;
-    char buf[BUFFER_SIZE];
+    char buf[BUFFER_SIZE], *random[5];
 
     // Create socket
-    if ((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) == -1) { 
+    if ((sockfd = socket(PF_INET6, SOCK_STREAM, 0)) == -1) { 
         fprintf(stderr, "Fail to create a socket\n");
         exit(1);
     }
@@ -26,8 +24,9 @@ int main() {
     // Bind an address to socket
     memset(&server, 0x00, sizeof(struct sockaddr_in));
     server.sin6_family = AF_INET6;
+    server.sin6_flowinfo = 0;
     server.sin6_port = htons(SERVER_TCP_PORT);
-    server.sin6_addr = in6addr_any;     // in6addr_any == htonl(INADDR_ANY)
+    server.sin6_addr = in6addr_any;
     if (bind(sockfd, (struct sockaddr *)&server, sizeof(server)) == -1) {
         fprintf(stderr, "Bind Error\n");
         exit(1);
@@ -36,45 +35,56 @@ int main() {
     if (listen(sockfd, 5) == -1) {
         fprintf(stderr, "Listen Error\n");
     }
+    printf("Listening..\n");
 
-
-    // IPv4
-    if ((ipv4_sd = socket(AF_INET6, SOCK_STREAM, 0)) == -1) {
-        fprintf(stderr, "Fail to create a socket - IPv4\n");
-        exit(1);
-    }
-    memset(&ipv4_server, 0x00, sizeof(struct sockaddr_in));
-    ipv4_server.sin6_family = AF_INET6;
-    ipv4_server.sin6_port = htons(SERVER_TCP_PORT);
-    ipv4_server.sin6_addr = in6addr_any;     // in6addr_any == htonl(INADDR_ANY)
-    if (bind(ipv4_sd, (struct sockaddr *)&ipv4_server, sizeof(ipv4_server)) == -1) {
-        fprintf(stderr, "Bind Error - IPv4\n");
-        exit(1);
-    }
-
-    if (listen(ipv4_sd, 5) == -1) {
-        fprintf(stderr, "Listen Error - IPv4\n");
-    }
-    if ((ipv4_new_sd = accept(ipv4_sd, (struct sockaddr *)&)))
-
+    idx = 0;
     for (int i = 0; i < 5; i++) {
         client_len = sizeof(client);
-        printf("clientlen : %d\n", client_len);
+        //printf("clientlen : %d\n", client_len);
         if ((new_sd = accept(sockfd, (struct sockaddr *)&client, (socklen_t *)&client_len)) == -1) {
             fprintf(stderr, "Accept Error\n");
             exit(1);
         }
         printf("Accept Client\n");
         bzero(buf, BUFFER_SIZE);
-        while ((n = read(new_sd, buf, BUFFER_SIZE)) > 0) {
+        if ((n = read(new_sd, buf, BUFFER_SIZE)) > 0) {
             buf[strlen(buf) - 2] = '\0';
-            printf("new_sd : %d => %s..\n", new_sd, buf);
-            
+            printf("Receive : %s\n", buf);
+            random[idx++] = strdup(buf);
         }
         close(new_sd);
+        printf("close\n");
     }
 
-    close(sockfd);
+    // send token to IPv4 client - message queue
+    typedef struct message {
+        long msg_type;
+        char data[BUFFER_SIZE];
+    } message;
+    int msqid;
 
+    message msg;
+    msg.msg_type = 1;
+    idx = 0;
+    for (int i = 0; i < 5; i++) {
+        if (i != 0)
+            msg.data[idx++] = ',';
+        for (int j = 0; random[i][j]; j++) {
+            msg.data[idx++] = random[i][j];
+        }
+    }
+    msg.data[idx] = '\0';
+
+    if ((msqid = msgget(1234, IPC_CREAT|0666)) == -1) {
+        fprintf(stderr, "Error msgget\n");
+        exit(1);
+    }
+    if (msgsnd(msqid, &msg, sizeof(msg), 0) == -1) {
+        fprintf(stderr, "Error msgsnd\n");
+        exit(1);
+    }
+    printf("message sent : %s..\n", msg.data);
+  
+    close(sockfd);
     return (0);
 }
